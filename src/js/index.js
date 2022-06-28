65ae4e22a4a26abb88d55bb34f57a363d635b380
h-kataoka <h-kataoka@bfp.co.jp>
2022-06-28 21:11:51 +0900

    開発環境を整える

---
+++ src/js/index.js
@@ -0,0 +1,59 @@
+import "./../scss/index.scss";
+
+class GourmandSearch {
+  constructor() {
+    this.status = document.querySelector(".status");
+    this.API_KEY = config.api;
+  }
+
+  init() {
+    this.getLocationInfo();
+  }
+
+  getLocationInfo() {
+    // 位置情報を取得
+    const geolocation = navigator.geolocation;
+    if (geolocation) {
+      this.status.textContent = "位置情報を取得中";
+      const localInfo = geolocation.getCurrentPosition(this.success);
+      console.log(localInfo);
+    } else {
+      this.status.textContent = "ブラウザがGeolocationに対応していません。";
+    }
+  }
+
+  success(position) {
+    // getCurrentPositionメソッドにわたす処理
+    const status = document.querySelector(".status");
+    console.log(position);
+    status.textContent = "";
+    const latitude = position.coords.latitude; // 緯度
+    const longitude = position.coords.longitude;
+    const localInfo = {
+      latitude: latitude,
+      longitude: longitude,
+    };
+    return localInfo;
+    // const searchData = this.fetchSearchData(localInfo);
+    // console.log(searchData);
+  }
+
+  test() {
+    console.log("yaaaaaaaaa");
+  }
+
+  fetchSearchData(localInfo) {
+    return fetch(
+      `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${this.API_KEY}&lat=${localInfo.latitude}&lng=${localInfo.longitude}`
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+const gourmandSearch = new GourmandSearch();
+gourmandSearch.init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,101 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
-  watch: true,
+  devtool: "source-map",
   entry: "./src/js/index.js",
   output: {
     path: __dirname + "/dist",
-    filename: "main.js",
+    filename: "[name].js",
   },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    watchFiles: ["./src/**/*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /apikey.js/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(c|sa|sc)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: true,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ["autoprefixer", { grid: true }],
+                  ["cssnano", { preset: "default" }],
+                ],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "pug-html-loader",
+            // コード整形オプション（これがないとminファイルのようになる）
+            options: {
+              pretty: true,
+              root: path.resolve(__dirname, "./src/pug"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: { drop_console: true }, // ビルド時に console.log を削除.
+        },
+      }),
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/pug/index.pug",
+      filename: "index.html",
+    }),
+  ],
 };
